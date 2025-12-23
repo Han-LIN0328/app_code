487a6d2486f25fe259d99901973eff079753d8ae
Han-LIN0328 <asddaniel950328@gmail.com>
2025-12-23 22:01:44 +0800

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    // ✅ 新增 OkHttp
+//    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+//
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
 }
